9c5deff796dce905374284127155821dd64d2485
jumbhood <bo.1020@hotmail.com>
2021-02-12 00:52:44 -0500

    Done!

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    title = "Open Your 2021 Red Evenlope!"
+    title = "DigitalEd - 2021 Red Evenlope"
     greetings = [
         "Wishing you happiness and prosperity; and an abundance of good fortune in your future!",
         "Happy New Year! The lucky star shines bright.",
+        "Good luck and great success in the coming New Year!",
         "Happy Lunar New Year 2021! May a river of gold flow into your pockets.",
         "May you have many good fortunes according to your wishes. Happy Spring Festival!",
         "Happy New Lunary Year to you! Good luck in the year ahead and may treasures fill your home.",
@@ -25,4 +26,4 @@ def home():
     return render_template('index.html', title = title, greeting = greeting, firework = firework)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug='True')
+    app.run(host='0.0.0.0', debug='True', port='80')
